2639111b755389f6a141b58fd7e75f921a47632b
Supratimd38 <120669005+Supratimd38@users.noreply.github.com>
2024-07-20 23:09:29 +0530

    First commit

---
+++ src/main/java/com/supratim/config/WebSocketHandler.java
@@ -0,0 +1,42 @@
+package com.supratim.config;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    private static List<WebSocketSession> sessions = new ArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        // Get the received message
+        String receivedMessage = message.getPayload();
+
+        // Broadcast the received message to all sessions
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen() && !session.getId().equals(webSocketSession.getId())) {
+                try {
+                    webSocketSession.sendMessage(new TextMessage(receivedMessage));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+}
